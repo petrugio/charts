d7d47514669ea34099150ae53aeda678943b5f5b
petrugio <petruchelban@gmail.com>
2023-07-28 00:05:22 +0000

    Create screener page

---
+++ js/screener.js
@@ -0,0 +1,13 @@
+const widgetOptions = {
+    "width": window.innerWidth,
+    "height": window.innerHeight,
+    "defaultColumn": "performance",
+    "defaultScreen": "unusual_volume",
+    "market": "america",
+    "showToolbar": true,
+    "colorTheme": "light",
+    "locale": "en"
+  };
+  
+  new TradingView.widget(widgetOptions);
+  
