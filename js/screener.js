581f7e31da9f837cb9707cba92fe5a3f929f6b0b
petrugio <petruchelban@gmail.com>
2023-10-04 15:19:42 +0000

    Add functionality to link the widgets

--- js/screener.js
+++ js/screener.js
@@ -1,13 +1,15 @@
-const widgetOptions = {
-    "width": window.innerWidth,
-    "height": window.innerHeight,
-    "defaultColumn": "performance",
-    "defaultScreen": "unusual_volume",
-    "market": "america",
-    "showToolbar": true,
-    "colorTheme": "light",
-    "locale": "en"
-  };
+document.addEventListener("DOMContentLoaded", function () {
+    const chartingIframe = document.getElementById("tradingview_de558");
   
-  new TradingView.widget(widgetOptions);
+    // Listen for messages from the screener widget
+    window.addEventListener("message", function (event) {
+      if (event.data && event.data.type === "symbolSelected") {
+        const selectedSymbol = encodeURIComponent(event.data.symbol);
+        const chartUrl = `https://petrugio.github.io/charts/screener.html?tvwidgetsymbol=${selectedSymbol}`;
+        
+        // Update the iframe's src with the selected symbol
+        chartingIframe.src = chartUrl;
+      }
+    });
+  });
   
